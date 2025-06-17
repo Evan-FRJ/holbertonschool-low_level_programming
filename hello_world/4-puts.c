b4e7988116c458d7568f788dca76deb3fa42a067
Evan-FRJ <11496@holbertonstudents.com>
2025-06-17 14:38:43 +0200

    print program

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main= Start of the program
+*Return (0) = no errors
+*/
+
+int main (void)
+{ 
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
+
