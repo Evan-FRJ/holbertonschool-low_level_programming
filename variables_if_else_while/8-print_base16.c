b994d45ab329ddf8db6623a68e1df4e6ac8cef93
Evan-FRJ <11496@holbertonstudents.com>
2025-06-20 11:10:54 +0200

    Print base 16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -14,7 +14,7 @@ int main(void)
 
 	for (number = 0; number < 10; number++)
 	{
-		printchar((number % 10) + '0');
+		putchar((number % 10) + '0');
 	}
 
 	for (letter = 'a'; letter <= 'f'; letter++)
