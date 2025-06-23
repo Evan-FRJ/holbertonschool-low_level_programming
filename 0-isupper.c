0f4cfbb427342a32a06520a96e39ebb4c1c5b370
Evan-FRJ <11496@holbertonstudents.com>
2025-06-23 14:09:41 +0200

    Detects uppercase

---
+++ 0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* isupper - checks for uppercase letter
+*
+* Return: 0 or 1
+*
+*/
+
+int is_upper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1)
+	else
+		return (0)
+}
+
