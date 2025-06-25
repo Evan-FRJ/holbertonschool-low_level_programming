4606e41931844e103edb558ebca3e0da89a736c2
Evan-FRJ <11496@holbertonstudents.com>
2025-06-25 09:28:08 +0200

    Prints a line n underscores long

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_line - Prints a line of n underscores long.
+ * @n: is the integer for the paramaters of the function
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+	 _putchar('\n');
+	}
+	else
+	_putchar('\n');
+}
